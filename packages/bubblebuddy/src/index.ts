6f6b899b6131a9d4d7b6ec29d6d9901540666578
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2026-09-18 11:00:53 -0700

    Make the minimum log level configurable via MINIMUM_LOG_LEVEL

--- packages/bubblebuddy/src/index.ts
+++ packages/bubblebuddy/src/index.ts
@@ -1,6 +1,6 @@
 import * as NodeRuntime from "@effect/platform-node/NodeRuntime";
 import * as NodeServices from "@effect/platform-node/NodeServices";
-import { Effect, Layer } from "effect";
+import { Config, Effect, Layer, References } from "effect";
 import { FetchHttpClient } from "effect/unstable/http";
 import { Otlp, OtlpSerialization } from "effect/unstable/observability";
 
@@ -40,4 +40,13 @@ const ObservabilityLayer = Otlp.layerFromConfig().pipe(
   Layer.provide(FetchHttpClient.layer),
 );
 
-NodeRuntime.runMain(Layer.launch(AppLayer).pipe(Effect.provide(ObservabilityLayer)));
+const LogLevelLayer = Layer.unwrap(
+  Config.LogLevel("MINIMUM_LOG_LEVEL").pipe(
+    Config.withDefault("Info"),
+    Effect.map((level) => Layer.succeed(References.MinimumLogLevel, level)),
+  ),
+);
+
+NodeRuntime.runMain(
+  Layer.launch(AppLayer).pipe(Effect.provide(Layer.merge(ObservabilityLayer, LogLevelLayer))),
+);
